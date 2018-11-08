e0c711046aeee1184a46ffbda29f51b860195b5e
Trieu Quoc Bao (FSU2.BU17) <BaoTQ5@fsoft.com.vn>
2018-11-08 17:02:27 +0700

    update nodemon

--- api-demo3/index.js
+++ api-demo3/index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const port = process.env.PORT || 3000;
  
 let server = http.createServer((req, res) => {
-    let html = `<h1>ádsadasdasd</h1>`;
+    let html = `<h1>bao bao 1111 1111</h1>`;
     // Add new code to test the live reload
     html += `<h2>As of: ${new Date()}</h2><br>`;
     // Iterate over `process.env` object and
