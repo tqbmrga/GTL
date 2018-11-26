4417f9ab734bda8a08033ac34511688ee534af02
Trieu Quoc Bao (FSU2.BU17) <BaoTQ5@fsoft.com.vn>
2018-11-26 11:28:12 +0700

    docker login

--- api-login/server.js
+++ api-login/server.js
@@ -1,16 +1,41 @@
-'use strict';
+// 'use strict';
 
-const express = require('express');
+// const express = require('express');
 
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+// // Constants
+// const PORT = 8080;
+// const HOST = '0.0.0.0';
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello bao bao\n');
+// // App
+// const app = express();
+// app.get('/', (req, res) => {
+//   res.send('Hello bao bao\n');
+// });
+
+// app.listen(PORT, HOST);
+// console.log(`Running on http://${HOST}:${PORT}`);
+
+
+const http = require('http');
+const port = process.env.PORT || 8080;
+ 
+let server = http.createServer((req, res) => {
+    let html = `<h1>bao bao 1111 1111</h1>`;
+    // Add new code to test the live reload
+    html += `<h2>As of: ${new Date()}</h2><br>`;
+    // Iterate over `process.env` object and
+    // print its values.
+    Object.keys(process.env).forEach((k) => {
+        html += `${k} = ${process.env[k]} <br>`
+    });
+    // Set the response status and response content 
+    // type header
+    res.writeHead(200, {
+        'content-type': 'text/html'
+    });
+    return res.end(html);
 });
-
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+ 
+// start listening
+server.listen(port);
+console.log(`Server running on port ${port}`);
